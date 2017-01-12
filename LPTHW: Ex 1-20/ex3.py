6d0b68260fa5ed9d8b672cfbc8678d458258d2ad
Nandigam Vijay Sai <vijaysai88nandigam@gmail.com>
2017-01-12 12:11:00 +0530

    Completed the third example

---
+++ LPTHW: Ex 1-20/ex3.py
@@ -0,0 +1,25 @@
+#print statment to understand the code
+print "I will now count my chickens:"
+#Caluclating the no of Hens
+print "Hens", 25 + 30 / 6
+#Caluclating the Roosters
+print "Roosters", 100 - 25 * 3 % 4
+
+print "Now I will count the eggs:"
+#Caluclating the eggs
+print 3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6
+#Check the validation of the expression
+print "Is it true that 3 + 2 < 5 - 7?"
+
+print 3 + 2 < 5 - 7
+
+print "What is 3 + 2?", 3 + 2
+print "What is 5 - 7?", 5 - 7
+
+print "Oh, that's why it's False."
+
+print "How about some more."
+
+print "Is it greater?", 5 > -2
+print "Is it greater or equal?", 5 >= -2
+print "Is it less or equal?", 5 <= -2
