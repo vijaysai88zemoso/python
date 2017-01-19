dde2d5fb97d3e42a8a87a3817c3b32b94ccc7f3f
Nandigam Vijay Sai <vijaysai.n@zemosolabs.com>
2017-01-19 17:44:33 +0530

    inclueded csv library to write csv to handele the possible scenarios

--- csv saver.py
+++ csv saver.py
@@ -1,5 +1,6 @@
 from sys import argv
 from ast import literal_eval
+import csv
 
 script, filename = argv
 
@@ -7,20 +8,15 @@ list_of_tuples = literal_eval(raw_input("Enter list of tuples : "))
 
 def writing_csv_file(list_of_tuples,filename):
     print "Opening the file..."
-    target = open(filename, 'w')
+    target = open(filename, 'wt')
 
     print "Taking the input entered and converted to string and writing in csv file ..."
-    for tuples in list_of_tuples:
-        for index, s in enumerate(tuples):
-            if "," in str(s):
-                target.write("\"" + str(s) + "\"")
-            else:
-                target.write(str(s))
-            if (index + 1) != len(tuples):
-                target.write(",")
-        target.write("\n")
-
-    target.close();
+    try:
+        writer = csv.writer(target)
+        for tuples in list_of_tuples:
+            writer.writerow(tuples)
+    finally:
+        target.close()
     print "Writing to csv is done"
     print "check the csv file"
 
